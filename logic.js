0f0c9048b3cae6468c8f3b4a5bfcaf3d780c8e5f
Tyler Bolty <tsbolty@gmail.com>
2020-04-07 11:37:05 -0600

    pushing java script for the first time

--- logic.js
+++ logic.js
@@ -1,21 +1,56 @@
 var todo = $("#input-area").val()
 
-var number = "12345".split()
+var listItems = []
 
+$("#priority-area").on("click", function(event){
+    event.preventDefault()
+    addItem()
+})
+$("#backburner-area").on("click", function(event){
+    event.preventDefault()
+    addItem()
+})
+
+$("#done-button").on("click", function(event){
+    event.preventDefault()
+    removeItem()
+})
 
 function addItem(){
-    for (i=0; i<number.length; i++){
+    for (i=0; i<listItems.length; i++){
         var li = $("<li>").text(todo).addClass("list-item").addId(`item${[i]}`)
+        var doneBtn = $("<button>").text("done").addId("done-button"+[i])
         todo.val("")
-        $("#to-do-list").append(li)
+        
+    if (listItems < 6){
+        listItems.push(todo.val)
+        $("#to-do-list").append(li, doneBtn)
+    } else {
+        alert("You can only have 5 items on the list")
     }
-}
+}}
 
 function removeItem(){
-    for (i=0; i<number.length; i++){
+    for (i=0; i<listItems.length; i++){
     $("#done-button").on("click", function(event){
         event.preventDefault()
         $(`#item${[i]}`).empty
+        $(`#done-button${[i]}`).empty
     })
-}
-}
+}}
+
+$("#how-to-play-button").on("click", function(event){
+    event.preventDefault()
+    $(".modal-content").style.display= "block";
+})
+
+$(".close").on("click", function(event){
+    event.preventDefault()
+    $(".modal-content").style.display= "none";
+})
+
+
+
+
+
+
