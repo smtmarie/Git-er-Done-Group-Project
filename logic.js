9f43e57f57424c2f991c9114b4af3c8787f6d803
Tyler Bolty <tsbolty@gmail.com>
2020-04-08 14:15:22 -0600

    dynamic list works perfectly. started progress on ajax calls and drink modal

--- logic.js
+++ logic.js
@@ -32,22 +32,28 @@ $("#backburner-button").on("click", function(event){
 // }
 
 function addBackburnerItem(){
-    var todo = $("#backburner-input-area").val()
+    var todo = $("#input-area").val()
     backburnerItems.push(todo)
     for (i=0; i<backburnerItems.length; i++){}
     if (backburnerItems.length < 6){
-        var li = $("<li>").text(todo).addClass("list-item").attr("id", `backburner-item${[i]}`)
-        var doneBtn = $("<button>").text("done").addClass("doneButton").attr("id", `backburner-done-button${[i]}`)
-        $("#backburner-input-area").val("").attr("style", "display: block")
-        $("#backburner-list").append(li, doneBtn)
+        var li = $("<li>").text(todo).addClass("list-item").attr("id", `backburner-item${i}`)
+        var doneBtn = $("<button>").text("done").addClass("doneButton").attr("id", i)
+        // var doneBtn = $("<label>").addClass("checkbox").text("Finished!")
+        // var checkBox= $("<input>").attr("type", "checkbox").attr("id", i)
+        $("#input-area").val("").attr("style", "display: block")
+        $("#to-do-list").append(li, doneBtn)
+        doneBtn.append(checkBox)
     } else {
-        $("#backburner-input-area").attr("style", "display: none")
+        $("#input-area").attr("style", "display: none")
     }
-    $(`#backburner-done-button${[i]}`).on("click", function(event){
-        event.preventDefault()
-        removeBackburnerItem()
-    })
 }
+
+$("#to-do-list").on("click", ".doneButton", function(event){
+    event.preventDefault()
+    console.log($(this).attr("id"))
+    removeBackburnerItem($(this).attr("id"))
+    
+})
 
 function addPriorityItem(){
     var todo = $("#priority-input-area").val()
@@ -65,6 +71,7 @@ function addPriorityItem(){
     $(`#priority-done-button${[i]}`).on("click", function(event){
         event.preventDefault()
         removePriorityItem()
+        
     })
 }
 
@@ -74,10 +81,11 @@ function removePriorityItem(){
     $(`#priority-done-button${[i]}`).remove()
 }
 
-function removeBackburnerItem(){
-    for (i=0; i<backburnerItems.length; i++){}
-    $(`#backburner-item${[i]}`).remove()
-    $(`#backburner-done-button${[i]}`).remove()
+function removeBackburnerItem(itemNumber){
+    $(`#backburner-item${itemNumber}`).remove()
+    $(`#${itemNumber}`).remove()
+    backburnerItems.pop()
+    console.log(backburnerItems)
 }
 
 // function removeItem(arr){
@@ -95,31 +103,58 @@ $("#how-to-play-button").on("click", function(event){
 $(".close").on("click", function(event){
     event.preventDefault()
     $(".modal-content").attr("style","display: none");
+    $(".drink-modal-content").attr("style","display: none");
 })
 
+$("#test-button").on("click", function(event){
+    event.preventDefault()
+    $("#drink-modal").attr("style", "display: block")
+})
 
-
+$("#alcoholic-button").on("click", function(){
+    event.preventDefault()
 $.ajax({
     url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
     method: "GET"
 }).then(function(response){
-    console.log(response)
     var drinkName= response.drinks[0].strDrink
     var drinkGlass= response.drinks[0].strGlass
     var drinkInstructions= response.drinks[0].strInstructions
     var drinkIngredients= [response.drinks[0].strIngredient1, response.drinks[0].strIngredient2, response.drinks[0].strIngredient3, response.drinks[0].strIngredient4, response.drinks[0].strIngredient5, response.drinks[0].strIngredient6, response.drinks[0].strIngredient7]
-    var heading= $("<h2>")
-    var ul=$("<ul>")
-    var li("<li>")
-    $("#alcoholic-button").on("click", function(){
-        event.preventDefault()
-        $(".drink-modal").append(heading, ul)
-        heading.text(drinkName)
-        ul.append(li).text(drinkGlass)
-        ul.append(li).text(drinkInstructions)
-        ul.append(li).text(drinkIngredients)
+        $("#drink-modal-header").text(drinkName)
+        $("#drink-glass").text(drinkGlass)
+        $("#drink-ingredients").text(drinkIngredients)
+        $("#drink-instructions").text(drinkInstructions)
+        console.log(drinkName)
+        console.log(drinkGlass)
+        console.log(drinkInstructions)
+        console.log(drinkIngredients)
     })
-
 })
+
+// if (backburnerItems.length===0){
+//     $("#drink-modal").attr("style", "display: block")
+// }
+
+// $("#nonalcoholic-button").on("click", function(){
+//     event.preventDefault()
+// $.ajax({
+//     url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+//     method: "GET"
+// }).then(function(response){
+//     console.log(response)
+//     var drinkName= response.drinks[0].strDrink
+//     var drinkGlass= response.drinks[0].strGlass
+//     var drinkInstructions= response.drinks[0].strInstructions
+//     var drinkIngredients= [response.drinks[0].strIngredient1, response.drinks[0].strIngredient2, response.drinks[0].strIngredient3, response.drinks[0].strIngredient4, response.drinks[0].strIngredient5, response.drinks[0].strIngredient6, response.drinks[0].strIngredient7]
+//     var ul=$("<ul>")
+//     var li=("<li>")
+//         $(".drink-modal-body").append(ul)
+//         $("#drink-modal-header").text(drinkName)
+//         ul.append(li).text(drinkGlass)
+//         ul.append(li).text(drinkInstructions)
+//         ul.append(li).text(drinkIngredients)
+//     })
+// })
 
 
